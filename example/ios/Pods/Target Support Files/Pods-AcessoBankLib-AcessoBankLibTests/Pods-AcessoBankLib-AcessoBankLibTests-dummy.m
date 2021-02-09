078353ac11cdec2ed4000f923ae75f24d93f109d
Wesley Dias <wdias@acesso.com>
2021-02-09 17:48:10 -0300

    update

--- example/ios/Pods/Target Support Files/Pods-AcessoBankLib-AcessoBankLibTests/Pods-AcessoBankLib-AcessoBankLibTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AcessoBankLib_AcessoBankLibTests : NSObject
-@end
-@implementation PodsDummy_Pods_AcessoBankLib_AcessoBankLibTests
-@end
--- example/ios/Pods/Target Support Files/Pods-AcessoBankLib/Pods-AcessoBankLib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AcessoBankLib : NSObject
-@end
-@implementation PodsDummy_Pods_AcessoBankLib
-@end
